919850ab675ed660b72b1d9b5e2d2ce494e840a3
Vladimir Dorofeev <90868048+Necromemeser@users.noreply.github.com>
2026-09-17 22:14:29 +0300

    feat(courses): add test domain model

---
+++ services/courses/internal/domain/test.go
@@ -0,0 +1,39 @@
+package domain
+
+import "time"
+
+type Test struct {
+	ID           int64
+	PageID       int64
+	PassingScore int
+	Questions    []TestQuestion
+}
+
+type TestQuestion struct {
+	ID       int64
+	TestID   int64
+	Question string
+	Position int
+	Answers  []TestAnswer
+}
+
+type TestAnswer struct {
+	ID         int64
+	QuestionID int64
+	Text       string
+	Position   int
+	IsCorrect  bool
+}
+
+type TestAttemptAnswer struct {
+	QuestionID int64
+	AnswerID   int64
+}
+
+type TestResult struct {
+	AttemptID    int64
+	Score        int
+	Passed       bool
+	PassingScore int
+	CompletedAt  time.Time
+}
